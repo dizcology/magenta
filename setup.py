0071c4199ad44b5ef878838f7d1cdd2d288534e6
Yu-Han Liu <yuhanliu@google.com>
2018-07-27 11:06:18 -0700

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(
+    name='magenta',
+    version='0.1dev',
+    packages=packages,
+    license='APACHE 2.0',
+)
